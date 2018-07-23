dac78c7ef22f22610883c4f8ba97767d4968b453
Alex Luton <aluton@gmail.com>
2018-07-23 19:45:51 +0100

    Add drag-drop container to image view

---
+++ static/gallery/image_upload.js
@@ -0,0 +1,24 @@
+/**
+Provide drag and drop support on image display pages
+*/
+
+document.addEventListener('drop', function(event) {
+    var data_input  = document.getElementById('image_upload_form').elements['data'];
+    data_input.files = event.dataTransfer.files;
+    event.preventDefault();
+});
+
+document.addEventListener('dragover', function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener('dragenter', function(event) {
+    var drop_container = document.getElementById('image_container');
+    drop_container.style.border = '10px solid';
+    event.preventDefault();
+});
+
+document.addEventListener('dragexit', function() {
+    var drop_container = document.getElementById('image_container');
+    drop_container.style.border = '0';
+});
