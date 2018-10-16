2f99cbc1a3b16c95804a388ef5ceaf436d992639
Alex Luton <aluton@2db.co.uk>
2018-10-16 11:41:30 +0100

    Add initial packaging files

---
+++ README.rst
@@ -0,0 +1,30 @@
+=================
+Starcross Gallery
+=================
+
+Starcross Gallery is a streamlined photo gallery Django app. Key features are:
+
+* Justified image grid display, as used on sites like Flickr
+* Easy drag and drop upload
+* Straightforward object model - All metadata is pulled from the file including title and exif data
+
+
+Quick start
+-----------
+
+1. Add "starcross-gallery" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'starcross-gallery',
+    ]
+
+2. Include the gallery URLconf in your project urls.py like this::
+
+    path('gallery/', include('starcross-gallery.urls')),
+
+3. Run `python manage.py migrate` to create the models.
+
+4. Start the development server and create any albums you required in http://127.0.0.1:8000/admin/. It's not necessary to create albums if you prefer just a single image feed
+
+5. Visit http://127.0.0.1:8000/gallery/ to access your image feed. Drag your images onto the page and you will see a flashing highlighted box .
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+requirements = ['django-imagekit==4.0.2', 'Pillow==5.0.0']
+
+setup(
+    name='django-starcross-gallery',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU LGPLv3',
+    description='A streamlined Django gallery app.',
+    long_description=README,
+    url='http://starcross.eu',
+    author='Alex Luton',
+    author_email='info+gallery@starcross.eu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=requirements,
+)
