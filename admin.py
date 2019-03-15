dac4994eaabd348b6e92f8e8853f560a11bb9c14
Alex Luton <aluton@2db.co.uk>
2019-03-15 16:41:29 +0000

    Add admin customisations to make editing images and albums easier

--- admin.py
+++ admin.py
@@ -1,6 +1,21 @@
-from django.contrib import admin
 from gallery.models import Image, Album
+from django.contrib import admin
+from imagekit.admin import AdminThumbnail
 
-# Gallery
-admin.site.register(Image)
-admin.site.register(Album)
+
+class ImageAdmin(admin.ModelAdmin):
+    admin_thumbnail = AdminThumbnail(image_field='data_thumbnail', template='gallery/admin/thumbnail.html')
+    list_display = ('title', 'admin_thumbnail', 'date_taken', 'date_uploaded')
+    list_filter = ('image_albums',)
+    list_per_page = 25
+    readonly_fields = ('admin_thumbnail',)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    filter_horizontal = ('images',)
+    raw_id_fields = ('highlight',)
+
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Album, AlbumAdmin)
+
