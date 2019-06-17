ffaee77ec98039cda5bb328b45574e02d494dfe3
starcross <aluton@gmail.com>
2019-06-17 20:06:13 +0100

    Add admin init back into test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,12 +4,20 @@ SECRET_KEY = 'fake-key'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery'
 )
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
 
 ROOT_URLCONF = 'tests.urls'
 
