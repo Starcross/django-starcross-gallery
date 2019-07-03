5643bb7cc25f00575619ed9e06e088f931867ccf
starcross <aluton@gmail.com>
2019-07-03 19:40:45 +0100

    Version 1.0.6 Add album links to image detail

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = ['django-imagekit==4.0.2', 'Pillow==5.0.0']
 
 setup(
     name='django-starcross-gallery',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     include_package_data=True,
     license='GNU LGPLv3',
